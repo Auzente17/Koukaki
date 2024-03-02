4ab6cf3d02aac4889aecd74cde5a0c143b12920d
Audry Perez <auzente01@gmail.com>
2024-03-02 16:50:04 +0100

    Ajoute de la section nomination

--- wp-content/themes/foce-child/front-page.php
+++ wp-content/themes/foce-child/front-page.php
@@ -18,51 +18,29 @@ get_header();
         </section>
 
 
-
-        <section id="story" class="story fade-in">
-            <h2>L'histoire</h2>
-            <article id="" class="story__article">
+        <section id="story" class="story">
+            <h2>
+                <span class="title">L'</span>
+                <span class="title">histoire</span>
+            </h2>
+            <article class="story__article">
                 <p><?php echo get_theme_mod('story'); ?></p>
             </article>
-            <?php
-            $args = array(
-                'post_type' => 'characters',
-                'posts_per_page' => -1,
-                'meta_key'  => '_main_char_field',
-                'orderby'   => 'meta_value_num',
 
-            );
-            $characters_query = new WP_Query($args);
-            ?>
-            <article id="characters">
-                <div class="main-character">
-                    <h3>Les personnages</h3>
-                    <?php
-                    $main_character = $characters_query->posts[0];
-                    echo '<figure>';
-                    echo get_the_post_thumbnail( $main_character->ID, 'full' );
-                    echo '<figcaption>'. $main_character->post_title . '</figcaption>';
-                    echo '</figure>';
-                    $characters_query->next_post();
-                    ?>
-                </div>
-                <div class="other-characters">
-                    <?php
-                    while ( $characters_query->have_posts() ) {
-                        $characters_query->the_post();
-                        echo '<figure>';
-                        echo get_the_post_thumbnail( get_the_ID(), 'full' );
-                        echo '<figcaption>';
-                        the_title();
-                        echo'</figcaption>';
-                        echo '</figure>';
-                    }
-                    ?>
-                </div>
-            </article>
-            <article id="place">
+            <!-- Section Characters déplacée dans "template-parts -->
+            <?php get_template_part('template-parts/characters'); ?>
+
+
+<!-- Section lieu -->
+           
+            <article id="place" class="story__place">
                 <div>
-                    <h3>Le Lieu</h3>
+                <h3>
+                    <span class="title">Le</span>
+                    <span class="title">Lieu</span>
+                </h3>
+
+
                     <p><?php echo get_theme_mod('place'); ?></p>
                 </div>
 
@@ -70,16 +48,20 @@ get_header();
         </section>
 
 
-        <section id="studio">
-            <h2>Studio Koukaki</h2>
+        <section id="studio" class="studio">
+            <h2>
+                <span class="title">Studio</span>
+                <span class="title">Koukaki</span>
+            </h2>
             <div>
-                <p>Acteur majeur de l’animation, Koukaki est un studio intégré fondé en 2012 qui créé, produit et distribue des programmes originaux dans plus de 190 pays pour les enfants et les adultes. Nous avons deux sections en activité : le long métrage et le court métrage. Nous développons des films fantastiques, principalement autour de la culture de notre pays natal, le Japon.</p>
-                <p>Avec une créativité et une capacité d’innovation mondialement reconnues, une expertise éditoriale et commerciale à la pointe de son industrie, le Studio Koukaki se positionne comme un acteur incontournable dans un marché en forte croissance. Koukaki construit chaque année de véritables succès et capitalise sur de puissantes marques historiques. Cette année, il vous présente “Fleurs d’oranger et chats errants”.</p>
+                <p class="studio__text">Acteur majeur de l’animation, Koukaki est un studio intégré fondé en 2012 qui créé, produit et distribue des programmes originaux dans plus de 190 pays pour les enfants et les adultes. Nous avons deux sections en activité : le long métrage et le court métrage. Nous développons des films fantastiques, principalement autour de la culture de notre pays natal, le Japon.</p>
+                <p class="studio__text">Avec une créativité et une capacité d’innovation mondialement reconnues, une expertise éditoriale et commerciale à la pointe de son industrie, le Studio Koukaki se positionne comme un acteur incontournable dans un marché en forte croissance. Koukaki construit chaque année de véritables succès et capitalise sur de puissantes marques historiques. Cette année, il vous présente “Fleurs d’oranger et chats errants”.</p>
             </div>
             </section>
 
             <!-- ajout de la section nomination templates-parts -->
         <?php get_template_part('template-parts/content', 'nomination'); ?>
+
     </main><!-- #main -->
 
 <?php
